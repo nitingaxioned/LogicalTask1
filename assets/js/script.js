b632b7a7b5d111df1f09d1766eb6b6e0d4ace83e
Nitin <niting@axioned.com>
2021-09-27 16:47:48 +0530

    6th Pattern Printed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 var i, j ,k ;
 
-document.write("Patter Program:<br><br>1)<br><br>");
+document.write("Pattern Program:<br><br>1)<br><br>");
 
 
 
@@ -26,7 +26,12 @@ document.write("<br><br>5)<br><br>");
 
 document.write("<br><br>6)<br><br>");
 
-
+for(i=1; i<7; i++){
+    for(j=1; j<=i; j++){
+        document.write("# ");
+    }
+    document.write("<br>");
+}
 
 document.write("<br><br>7)<br><br>")
 

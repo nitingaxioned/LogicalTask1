2e4c8d2e05fdce96b7243a7fd93750e823f6b00d
Nitin <niting@axioned.com>
2021-09-27 16:48:22 +0530

    3rd Pattern Printed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,12 @@ document.write("<br><br>2)<br><br>");
 
 document.write("<br><br>3)<br><br>");
 
-
+for(i=1; i<4; i++){
+    for(j=1; j<=i; j++){
+        document.write(j**i+" ");
+    }
+    document.write("<br>");
+}
 
 document.write("<br><br>4)<br><br>");
 

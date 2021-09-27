a3957b7911096142b3131a44f153536ff7e61478
Nitin <niting@axioned.com>
2021-09-27 16:46:22 +0530

    title and Numbering adeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,33 @@
 
 */
 
+var i, j ,k ;
+
+document.write("Patter Program:<br><br>1)<br><br>");
+
+
+
+document.write("<br><br>2)<br><br>");
+
+
+
+document.write("<br><br>3)<br><br>");
+
+
+
+document.write("<br><br>4)<br><br>");
+
+
+
+document.write("<br><br>5)<br><br>");
+
+
+
+document.write("<br><br>6)<br><br>");
+
+
+
+document.write("<br><br>7)<br><br>")
 
 
 
